3ec0a4ec4eb65323f6537debe6c5c219063807c0
LiuCan <liucan@chinarecrm.com.cn>
2021-01-11 15:58:45 +0800

    :art: init

---
+++ src/main/java/com/lim/algs4/Algs4Application.java
@@ -0,0 +1,13 @@
+package com.lim.algs4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Algs4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Algs4Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/lim/algs4/Algs4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lim.algs4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Algs4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
